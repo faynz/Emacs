64315c07c82aacc4a2bbe2b3ae0fc4fde4a8e3f6
Estefan VC <estefan.vega-calcada@polymtl.ca>
2019-09-11 12:58:47 -0400

    Colors and dired

--- init.el
+++ init.el
@@ -22,3 +22,11 @@
 
 
 (put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-display-line-numbers-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
